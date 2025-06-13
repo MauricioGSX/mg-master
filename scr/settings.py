6ecadd7b2d7935e09372156057bc9045ee124eee
MauricioGSX <146774282+MauricioGSX@users.noreply.github.com>
2025-06-13 19:58:18 -0400

    Update settings.py

--- scr/settings.py
+++ scr/settings.py
@@ -21,6 +21,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://mg-master.onrender.com',
+]
+
 # Use custom user model
 AUTH_USER_MODEL = 'base.CustomUser'
 LOGIN_URL = '/login/'
